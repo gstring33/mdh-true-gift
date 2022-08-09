4b5e2bdc6ddf2cb7db56597b709668201573fa1e
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-09 11:25:05 +0200

    Create Members component

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,10 +1,61 @@
 <template>
-  <h3>Members (6)</h3>
-  <div>
+  <div class="container">
+    <div class="row px-4 py-4 border bg-ligh">
+      <h1 class="display-6 mb-3">Teilnehmer (9)</h1>
+      <hr/>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Hallo Martin!</strong> You should check in on some of those fields below.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <div class="row mb-4">
+        <div class="row align-items-start mb-md-3">
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            John D.
+          </div>
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Patrick C.
+          </div>
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Steven V.
+          </div>
+        </div>
+        <div class="row align-items-center mb-md-3">
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Emily V.
+          </div>
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Karl Y.
+          </div>
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Ella T.
+          </div>
+        </div>
+        <div class="row align-items-end">
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Stefany L.
+          </div>
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Julia S.
+          </div>
+          <div class="col col-12 col-md-4 lead">
+            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+            Sophie K.
+          </div>
+        </div>
+      </div>
+      <div class="d-grid mx-auto d-md-flex">
+        <button class="btn btn-primary disabled" type="button">Partner auswählen</button>
+      </div>
+    </div>
   </div>
-  <button>
-    Select your partner
-  </button>
 </template>
 
 <script></script>
--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg bg-light mb-5">
         <div class="container-fluid">
             <router-link class="navbar-brand" to="/">Logo</router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
