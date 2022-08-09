d40299ac0b288badaf892786e96554170628eab9
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-09 12:36:11 +0200

    Implement List component

---
+++ src/components/List.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container mt-5">
+        <div class="card mb-3 ">
+            <div class="card-header display-6 py-3">Meine Liste</div>
+            <div class="card-body">
+                <div class="alert alert-warning" role="alert">
+                    Du hast noch keine Wünsche hinzugefügt
+                </div>
+                <ol class="list-group mb-4">
+                    <li class="list-group-item">
+                        <div class="col col-12 mt-3">
+                            <div class="fw-bold">1. Subheading</div>
+                            Lorem ipsum dolor sit amet, sed diam yonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </div>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
+                            <div class="btn-group " role="group" aria-label="Basic mixed styles example">
+                                <button type="button" class="btn btn-secondary btn-sm">Bearbeiten</button>
+                                <button type="button" class="btn btn-danger btn-sm">Entfernen</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="col col-12 mt-3">
+                            <div class="fw-bold">2. Subheading</div>
+                            Lorem ipsum dolor sit amet, sed diam yonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+                        </div>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
+                            <div class="btn-group " role="group" aria-label="Basic mixed styles example">
+                                <button type="button" class="btn btn-secondary btn-sm">Bearbeiten</button>
+                                <button type="button" class="btn btn-danger btn-sm">Entfernen</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="col col-12 mt-3">
+                            <div class="fw-bold">3. Subheading</div>
+                            Lorem ipsum dolor sit amet.
+                        </div>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
+                            <div class="btn-group " role="group" aria-label="Basic mixed styles example">
+                                <button type="button" class="btn btn-secondary btn-sm">Bearbeiten</button>
+                                <button type="button" class="btn btn-danger btn-sm">Entfernen</button>
+                            </div>
+                        </div>
+                    </li>
+                </ol>
+                <button type="button" class="btn btn-primary">Einen Wunsch hinzufügen</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,11 @@
 <template>
   <Members />
+  <List />
 </template>
 
 <script setup>
 import Members from "@/components/Members.vue";
+import List from "@/components/List.vue";
 </script>
 
 <style scoped></style>
