963b889a54f48762100a0ed231059b308fb13376
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-09 12:35:52 +0200

    Transform Members component as a card

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,58 +1,53 @@
 <template>
   <div class="container">
-    <div class="row px-4 py-4 border bg-ligh">
-      <h1 class="display-6 mb-3">Teilnehmer (9)</h1>
-      <hr/>
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Hallo Martin!</strong> You should check in on some of those fields below.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-      <div class="row mb-4">
-        <div class="row align-items-start mb-md-3">
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            John D.
+    <div class="card mb-3">
+      <div class="card-header display-6 py-3">Teilnehmer (9)</div>
+      <div class="card-body">
+        <div class="row mb-4">
+          <div class="row align-items-start mb-md-3">
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              John D.
+            </div>
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Patrick C.
+            </div>
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Steven V.
+            </div>
           </div>
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Patrick C.
+          <div class="row align-items-center mb-md-3">
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Emily V.
+            </div>
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Karl Y.
+            </div>
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Ella T.
+            </div>
           </div>
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Steven V.
-          </div>
-        </div>
-        <div class="row align-items-center mb-md-3">
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Emily V.
-          </div>
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Karl Y.
-          </div>
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Ella T.
-          </div>
-        </div>
-        <div class="row align-items-end">
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Stefany L.
-          </div>
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Julia S.
-          </div>
-          <div class="col col-12 col-md-4 lead">
-            <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
-            Sophie K.
+          <div class="row align-items-end">
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Stefany L.
+            </div>
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Julia S.
+            </div>
+            <div class="col col-12 col-md-4 lead">
+              <img style="width: 60px; position: relative; bottom: 4px;" src="@/assets/images/user-profil.png" alt="user-profil">
+              Sophie K.
+            </div>
           </div>
         </div>
-      </div>
-      <div class="d-grid mx-auto d-md-flex">
-        <button class="btn btn-primary disabled" type="button">Partner auswählen</button>
+        <button type="button" class="btn btn-primary">Einen Partner auswählen</button>
       </div>
     </div>
   </div>
