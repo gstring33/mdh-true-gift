9d77b9bb581e6622a07743abddf21ee3cb69c651
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-11 11:20:39 +0200

    display modal when deleting a wish

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,39 +7,17 @@
                     Du hast noch keine Wünsche hinzugefügt
                 </div>
                 <ol class="list-group mb-4">
-                    <li class="list-group-item">
+                    <li v-for="(item, index) in wishes" class="list-group-item">
                         <div class="col col-12 mt-3">
-                            <div class="fw-bold">1. Subheading</div>
+                            <div class="fw-bold">{{ item.title}}</div>
                             Lorem ipsum dolor sit amet, sed diam yonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                         </div>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
                             <div class="btn-group " role="group" aria-label="Basic mixed styles example">
                                 <button type="button" class="btn btn-secondary btn-sm">Bearbeiten</button>
-                                <button type="button" class="btn btn-danger btn-sm">Entfernen</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="col col-12 mt-3">
-                            <div class="fw-bold">2. Subheading</div>
-                            Lorem ipsum dolor sit amet, sed diam yonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
-                        </div>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
-                            <div class="btn-group " role="group" aria-label="Basic mixed styles example">
-                                <button type="button" class="btn btn-secondary btn-sm">Bearbeiten</button>
-                                <button type="button" class="btn btn-danger btn-sm">Entfernen</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="col col-12 mt-3">
-                            <div class="fw-bold">3. Subheading</div>
-                            Lorem ipsum dolor sit amet.
-                        </div>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
-                            <div class="btn-group " role="group" aria-label="Basic mixed styles example">
-                                <button type="button" class="btn btn-secondary btn-sm">Bearbeiten</button>
-                                <button type="button" class="btn btn-danger btn-sm">Entfernen</button>
+                                <button @click="openModal(item.title, `wish-${index}`)" type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal" ref="wisch1">
+                                    Entfernen
+                                </button>
                             </div>
                         </div>
                     </li>
@@ -47,10 +25,40 @@
                 <button type="button" class="btn btn-primary">Einen Wunsch hinzufügen</button>
             </div>
         </div>
+        <Teleport to="#modals">
+            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            {{ modalTitle }} - {{ modalId }}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Nein</button>
+                            <button type="button" class="btn btn-danger">Entfernen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Teleport>
     </div>
 </template>
 
-<script>
+<script setup>
+    import {ref} from 'vue'
+
+    const modalTitle = ref(null)
+    const modalId = ref(null)
+
+    const wishes = ref([
+        {title: "wisch1"},
+        {title: "wisch2"},
+        {title: "wisch3"}
+    ])
+
+    const openModal = (title, id) => {
+        modalTitle.value = title
+        modalId.value = id
+    }
 </script>
 
 <style scoped>
