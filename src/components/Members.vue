e46ce75d5ec58b4a735b64160097a602899d297b
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-11-11 09:43:42 +0100

    fetch and display users

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -4,12 +4,12 @@
       <b><font-awesome-icon :icon="['fas', 'triangle-exclamation']" class="me-1"/> Noch ein wenig Geduld...</b> Ella hat ihre Liste noch nicht veröffentlicht. Bitte logge dich später erneut ein, um Ellas Liste zu sehen.
     </div>
     <div class="card mb-3">
-      <div class="card-header py-3 lead">Teilnehmer (9)</div>
+      <div class="card-header py-3 lead">Teilnehmer ({{ total }})</div>
       <div class="card-body">
         <div class="row mb-4">
-          <div v-for="(partnerLine, index) in partnersNormalized" class="row mb-md-3" :class="index == partnerLine.length - 1 ? 'align-items-end' : 'align-items-start'">
-            <div v-for="partner in partnerLine" class="col col-12 col-md-4">
-              <MemberItem :partner="partner" :isSelectionAllowed="isSelectionAllowed" :image="partner.img"/>
+          <div v-for="(userLine, index) in users" class="row mb-md-3" :class="index == userLine.length - 1 ? 'align-items-end' : 'align-items-start'">
+            <div v-for="user in userLine" class="col col-12 col-md-4">
+              <MemberItem :user="user"/>
             </div>
           </div>
         </div>
@@ -23,8 +23,32 @@
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import {ref, computed, defineProps, reactive} from "vue";
 import MemberItem from './MemberItem.vue'
+import { onBeforeMount } from "vue";
+import { fetcher } from "@/helpers/fetcher.js";
+
+const total = ref(null)
+const users = ref(null)
+
+onBeforeMount(async () => {
+  try {
+    const data = await fetcher.get(import.meta.env.VITE_API_BASE_URL + '/api/dashboard/users')
+    const usersArr  = []
+    let userLine = [];
+    data.users.forEach((p) => {
+      userLine.push(p)
+      if(userLine.length === 3 || (userLine.length === data.total && data.total < 3) ) {
+        usersArr.push(userLine)
+        userLine = []
+      }
+    })
+    total.value = data.total
+    users.value = usersArr
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 // ---------- Refs
 const isPartnerNotSelected = ref(false)
@@ -44,17 +68,17 @@ const partners = ref([
 ])
 
 // ---------- Computed
-const partnersNormalized = computed(() => {
-  const partnersArr  = []
-  let partnerLine = [];
-  partners.value.forEach((p) => {
-    partnerLine.push(p)
-    if(partnerLine.length === 3) {
-      partnersArr.push(partnerLine)
-      partnerLine = []
+const usersNormalized = computed(() => {
+  const usersArr  = []
+  let userLine = [];
+  users.value.forEach((p) => {
+    userLine.push(p)
+    if(userLine.length === 3) {
+      usersArr.push(userLine)
+      userLine = []
     }
   })
 
-  return partnersArr
+  return usersArr
 })
 </script>
