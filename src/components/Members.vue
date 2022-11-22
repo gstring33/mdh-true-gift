767bab13520c69cbed78e96640964cd3f670aaa5
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-11-22 11:29:53 +0100

    Initialize data in Dashboard view

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -4,7 +4,7 @@
       <b><font-awesome-icon :icon="['fas', 'triangle-exclamation']" class="me-1"/> Noch ein wenig Geduld...</b> Ella hat ihre Liste noch nicht veröffentlicht. Bitte logge dich später erneut ein, um Ellas Liste zu sehen.
     </div>
     <div class="card mb-3">
-      <div class="card-header py-3 lead">Teilnehmer ({{ total }})</div>
+      <div class="card-header py-3 lead">Teilnehmer ({{ totalUsers }})</div>
       <div class="card-body">
         <div class="row mb-4">
           <div v-for="(userLine, index) in users" class="row mb-md-3" :class="index == userLine.length - 1 ? 'align-items-end' : 'align-items-start'">
@@ -23,35 +23,16 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
+
 import MemberItem from './MemberItem.vue'
-import { onBeforeMount } from "vue";
-import { fetcher } from "@/helpers/fetcher.js";
 import { useAuthStore } from "@/stores/auth.store.js";
 
-const total = ref(null)
-const users = ref(null)
 const { user } = useAuthStore();
 
-onBeforeMount(async () => {
-  try {
-    const data = await fetcher.get(import.meta.env.VITE_API_BASE_URL + '/api/dashboard/users')
-    const usersArr  = []
-    let userLine = [];
-    data.users.forEach((p) => {
-      userLine.push(p)
-      if(userLine.length === 3 || (userLine.length === data.total && data.total < 3) ) {
-        usersArr.push(userLine)
-        userLine = []
-      }
-    })
-    total.value = data.total
-    users.value = usersArr
-
-  } catch (error) {
-      const { logout } = useAuthStore();
-      logout()
-  }
+const props = defineProps({
+  users: Object,
+  totalUsers: Number
 })
+
 
 </script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
   <Navbar />
-  <Members />
+  <Members :totalUsers="total" :users="users"/>
   <List />
 </template>
 
@@ -8,6 +8,34 @@
 import Navbar from "@/components/common/Navbar.vue";
 import Members from "@/components/Members.vue";
 import List from "@/components/List.vue";
+import { fetcher } from "@/helpers/fetcher.js";
+import {onBeforeMount, ref} from "vue";
+import { useAuthStore } from "@/stores/auth.store.js";
+
+const total = ref(null)
+const users = ref(null)
+
+onBeforeMount(async () => {
+  try {
+    const data = await fetcher.get(import.meta.env.VITE_API_BASE_URL + '/api/dashboard/users')
+    const usersArr  = []
+    let userLine = [];
+    data.users.forEach((p) => {
+      userLine.push(p)
+      if(userLine.length === 3 || (userLine.length === data.total && data.total < 3) ) {
+        usersArr.push(userLine)
+        userLine = []
+      }
+    })
+    total.value = data.total
+    users.value = usersArr
+
+  } catch (error) {
+    const { logout } = useAuthStore();
+    logout()
+  }
+})
+
 </script>
 
 <style scoped></style>
