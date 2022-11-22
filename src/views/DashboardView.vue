feca18092e02b8ac51dc19423acf0f46afc6b694
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-11-22 11:49:10 +0100

    Implement spinner to init the app

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,15 @@
 <template>
   <Navbar />
-  <Members :totalUsers="total" :users="users"/>
-  <List />
+  <div v-if="!ready" class="d-flex justify-content-center">
+    <div class="spinner-border m-5 text-primary" style="width: 3rem; height: 3rem;" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <div v-else>
+
+    <Members :totalUsers="total" :users="users"/>
+    <List />
+  </div>
 </template>
 
 <script setup>
@@ -14,20 +22,23 @@ import { useAuthStore } from "@/stores/auth.store.js";
 
 const total = ref(null)
 const users = ref(null)
+const ready = ref(false)
 
 onBeforeMount(async () => {
   try {
-    const data = await fetcher.get(import.meta.env.VITE_API_BASE_URL + '/api/dashboard/users')
+    const usersData = await fetcher.get(import.meta.env.VITE_API_BASE_URL + '/api/dashboard/users')
+    ready.value = true
+
     const usersArr  = []
     let userLine = [];
-    data.users.forEach((p) => {
+    usersData.users.forEach((p) => {
       userLine.push(p)
-      if(userLine.length === 3 || (userLine.length === data.total && data.total < 3) ) {
+      if(userLine.length === 3 || (userLine.length === usersData.total && usersData.total < 3) ) {
         usersArr.push(userLine)
         userLine = []
       }
     })
-    total.value = data.total
+    total.value = usersData.total
     users.value = usersArr
 
   } catch (error) {
