1699252bf1f66ccb8651e3e18cc08c5b5ab94673
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-12-04 18:56:21 +0100

    Display logout btn

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -4,18 +4,30 @@
       <router-link class="navbar-brand" to="/">
         <img :src="logoUrl" width="110">
       </router-link>
-      <ProfilMenu></ProfilMenu>
+      <!--<ProfilMenu></ProfilMenu>-->
+      <a type="button" class="btn btn-outline-dark btn-sm" @click="logout">
+        <font-awesome-icon :icon="['fas', 'arrow-up-right-from-square']"/> Logout
+      </a>
     </div>
   </nav>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
-import ProfilMenu from '@/components/common/ProfilMenu.vue';
+//import ProfilMenu from '@/components/common/ProfilMenu.vue';
 // eslint-disable-next-line
 import { Dropdown } from "bootstrap";
+import { useAuthStore } from "@/stores/auth.store.js";
+
 
 const logoUrl = new URL('../../assets/images/true-gift-logo.png', import.meta.url).href
+
+const auth = useAuthStore()
+
+const logout = function () {
+  auth.logout()
+}
+
 </script>
 
 <style lang="scss" scoped>
